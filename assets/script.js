71a1c0b7c82144989d551bead2b2c1448dea9216
Sergio Gomez <sergiogomez100000@gmail.com>
2021-03-01 16:36:03 -0700

    just in case

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,16 @@
 // Assignment Code
 // # = id, . = class
-//setting up variable named generateBtn, grabs first "generate" id from the document.
+//setting up variable named generateBtn, grabs "generate" id from the document.
 var generateBtn = document.querySelector("#generate");
 // setting up variables with array values
+// Initialize global variables
 // array of data-type numbers
+// uppercase, lowercase, numbers, characters
 var numbers = [0,1,2,3,4,5,6,7,8,9];
-// array of data-type string
+// array of data-type strings because in parenthesis split with commas
 var specialCharacters = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+"];
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
-// Initialize global variables
-// uppercase, lowercase, numbers, characters
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
